fdd4f2f5adaa02923c60c69e045b779d696e0c60
sfincks <sfincks666@gmail.com>
2023-03-09 12:29:13 +0500

    keyboard input added

--- Program.cs
+++ Program.cs
@@ -2,14 +2,16 @@
 
 // string[] array = { "я", "так", "хочу", "Чтобы", "лето", "не", "кончалось", "Чтоб", "оно", "за", "мною", "мчалось", "За", "мною", "вслед" };
 // { "я", "так", "не", "оно", "за",  "За"}
-string[] array = { "Это", "вам", "не", "это", "Обязательно", "бахнем", "И", "не", "раз", "Весь", "мир", "в", "труху", "Но", "потом" };
+// string[] array = { "Это", "вам", "не", "это", "Обязательно", "бахнем", "И", "не", "раз", "Весь", "мир", "в", "труху", "Но", "потом" };
+// [Это вам не это И не раз мир в Но]
+string[] array = getArray();
 string[] newArr = shortenArray(array, 3);
-System.Console.WriteLine("[" + string.Join(", ", newArr) + "]");
+System.Console.WriteLine("[" + string.Join(" ", newArr) + "]");
 
 string[] shortenArray(string[] array, int maxSymbol)
 {
     int arrleng = 0;
-    for (int i = 0; i < array.Length - 1; i++)
+    for (int i = 0; i < array.Length ; i++)
     {
         if (array[i].Length <= maxSymbol)
         {
@@ -19,7 +21,7 @@ string[] shortenArray(string[] array, int maxSymbol)
     }
     string[] array2 = new string[arrleng];
     int count = 0;
-    for (int i = 0; i < array.Length - 1; i++)
+    for (int i = 0; i < array.Length ; i++)
     {
         if (array[i].Length <= maxSymbol)
         {
@@ -28,6 +30,21 @@ string[] shortenArray(string[] array, int maxSymbol)
         }
     }
     return array2;
+}
+
+string[] getArray()
+{
+    System.Console.WriteLine("Введите количество строк");
+    int Length = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Введите строку №{i + 1}");
+        array[i] = Console.ReadLine();
+    }
+    System.Console.WriteLine("Вы ввели следующие строки");
+    System.Console.WriteLine("[" + string.Join(" ", array) + "]");
+    return array;
 }
 
 
